c721c84e5cc9aa19fa5d6e868ed077cc8adb8702
Mordtimer <filipgab99@gmail.com>
2020-03-30 21:44:46 +0200

    first commit

---
+++ Calculator/Cal.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+
+namespace Calculator {
+    class Cal {
+
+        public string Equation { get; set; }
+
+        #region methods
+        public string Calculate() {
+            DataTable result = new DataTable();
+            string str_res;
+            double double_res;
+            
+            GetFinalForm();
+            try {
+                str_res = Convert.ToString(result.Compute(Equation, String.Empty));
+                double_res = Convert.ToDouble(str_res);
+                str_res = double_res.ToString();
+            }
+            catch(SyntaxErrorException) {
+                return "Are U OK?";
+            }
+            if(double_res == double.PositiveInfinity || double_res == double.NegativeInfinity)
+                return "No way!";
+
+            return str_res;
+        }
+
+        public void GetFinalForm() {
+
+            //Close all unclosed brackets
+            int open = 0, closed = 0;
+            foreach(char el in Equation) {
+                if(el == ')') closed++;
+                else if(el == '(') open++;
+            }
+            for(int i = 0; i < (open - closed); i++)
+                AddCharacter(')');
+
+            //Remove last character in case of being bad operator
+            if(non_brackets_operators.Contains(Equation[Equation.Length - 1]))
+                RemoveLast();
+        }
+
+        public void AddCharacter(char element) {
+            if(IsProper(ref element))
+                Equation += element;
+            else Equation = Equation;
+        }
+
+        public void ResetEquation() {
+            Equation = "";
+        }
+
+        public void RemoveLast() {
+            if (Equation.Length != 0)
+                Equation = Equation.Remove(Equation.Length - 1);
+        }
+
+        public bool IsProper(ref char element) {
+
+            switch(element) {
+                
+                case ')':
+                    if(Equation[Equation.Length - 1] == '(')
+                        return false;
+                    int open = 0, closed = 0;
+                    foreach(char el in Equation) {
+                        if(el == ')') closed++;
+                        else if(el == '(') open++;
+                    }
+                    if(open <= closed) return false;
+                    break;
+                
+                case '(':
+                    if(Equation == null || Equation.Length == 0) break;
+
+                    if(!operators.Contains(Equation[Equation.Length-1]) 
+                        || Equation[Equation.Length - 1] == '.' || Equation[Equation.Length - 1] == ')') 
+                        return false;     
+                    break;
+                
+                case ',':
+                    element = '.';
+                    break;
+
+                case '.':
+                    if(Equation == null || Equation.Length == 0)
+                        AddCharacter('0'); 
+                    else if(operators.Contains(Equation[Equation.Length - 1]) || Equation[Equation.Length - 1] == '.')
+                        return false;
+                    break;
+                
+                case '+':
+                case '-':
+                    if(Equation == null || Equation.Length == 0)
+                        return false;
+                    else if(Equation[Equation.Length - 1] == '.')
+                        return false;
+                    else if(non_brackets_operators.Contains(Equation[Equation.Length - 1]))
+                        RemoveLast();
+                    break;
+                
+                case '/':
+                case '*':
+                    if(Equation == null || Equation.Length == 0)
+                        return false;
+                    else if(Equation[Equation.Length - 1] == '.' || Equation[Equation.Length - 1] == '(')
+                        return false;
+                    else if(non_brackets_operators.Contains(Equation[Equation.Length - 1]))
+                          RemoveLast();
+                    break;
+
+                default:
+                    if(!(Equation == null || Equation.Length == 0))
+                        if(Equation[Equation.Length - 1] == ')')
+                            return false;
+                    break;
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region operator lists
+
+        private readonly List<char> operators = new List<char> {
+
+            '-', '+', '*', '/', ')', '(',
+        };
+
+        private readonly List<char> non_brackets_operators = new List<char> {
+
+            '-', '+', '*', '/'
+        };
+
+        #endregion
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Cal calcul = new Cal();
+        
+        public MainWindow(){
+           
+            InitializeComponent();
+        }
+                
+        private void Update() {
+            
+            TextBlockExpression.Text = calcul.Equation;            
+        }
+
+        #region actions
+        private void ButtonClick(object sender, RoutedEventArgs e) {
+
+            char character;
+            string button_str = Convert.ToString((sender as Button).Tag);
+            if(char.TryParse(button_str, out character)) {
+                calcul.AddCharacter(character);
+                Update();
+            }       
+        }
+
+        private void ButtonClearAllClick(object sender, RoutedEventArgs e) {
+
+            calcul.ResetEquation();            
+            Update();
+            TextBlockResult.Text = "";
+        }
+
+        private void ButtonDelClick(object sender, RoutedEventArgs e) {
+
+            calcul.RemoveLast();
+            Update();
+        }
+
+        private void ButtonEqual_Click(object sender, RoutedEventArgs e) {
+            TextBlockResult.Text = calcul.Calculate();
+            Update();
+        }
+        #endregion
+    }
+}
